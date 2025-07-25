09781b5a847f0fa3266a30568b7bc8726cb64b4f
Aleksa Aleksic <70874227+aleksa3009@users.noreply.github.com>
2025-07-25 17:16:50 +0200

    Add DriverFactory class

---
+++ src/main/java/utils/DriverFactory.java
@@ -0,0 +1,43 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            String browser = ConfigReader.get("browser");
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new RuntimeException("Unsupported browser: " + browser);
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(
+                    Integer.parseInt(ConfigReader.get("implicitWait"))
+            ));
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
